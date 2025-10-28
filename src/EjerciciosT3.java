3eaf8198a507c5d234a973ef68672e4dd0ca9f2e
adrianmartinezhurtado <adrianmartinezhurtado25@gmail.com>
2025-10-28 20:42:22 +0100

    Summary

---
+++ src/EjerciciosT3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class EjerciciosT3 {
+    Scanner lectorTeclado;
+
+
+    //Desarrolla un programa que pida un número al usuario
+    // y determine si es positivo, negativo o cero.
+    // Muestra el resultado por consola.
+    public void Ejercicio2(){
+        Scanner lectorTeclado = new Scanner(System.in);
+        System.out.println("Introduce un número:");
+        int numero = lectorTeclado.nextInt();
+        //positivo, negativo
+        if (numero >= 0) {
+            System.out.println("El número es positivo.");
+        } else {
+            System.out.println("El número es negativo");
+        }
+    }
+
+    public void Ejercicio3(){
+        //Escribe un programa que pida la nota de un examen (entre 0 y 10)
+        //y determine si el alumno ha aprobado (nota mayor o igual a 5) o ha suspendido.}
+        Scanner lectorTeclado = new Scanner(System.in);
+        System.out.println("Introduce la nota de tu examen:");
+        float nota = lectorTeclado.nextFloat();
+        // Aprobado o suspenso
+        if (nota <= 4.99){
+            System.out.println("Tu examen está suspenso.");
+        } else {
+            System.out.println("!Enhorabuena¡ Has aprobado");
+        }
+        }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        EjerciciosT3 variableEjercicios = new EjerciciosT3();
+        //variableEjercicios.Ejercicio2();
+        variableEjercicios.Ejercicio3();
+
+    }
+}
