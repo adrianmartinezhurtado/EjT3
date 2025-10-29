6d7ad6ba7a08f8279a580b3572edefc9cd2c9d3a
adrianmartinezhurtado <adrianmartinezhurtado25@gmail.com>
2025-10-29 20:56:12 +0100

    Update EjerciciosT3.java

--- src/EjerciciosT3.java
+++ src/EjerciciosT3.java
@@ -71,7 +71,7 @@ public class EjerciciosT3 {
 
     public void Ejercicio7(){
         //Crea un programa que pida una nota numérica (0-10) y determine la calificación: 
-        // Suspenso (0-4), Aprobado (5-6), Notable (7-8) o Sobresaliente (9-10). hg
+        // Suspenso (0-4), Aprobado (5-6), Notable (7-8) o Sobresaliente (9-10). 
         Scanner lectorTeclado = new Scanner(System.in);
         System.out.println("Introduce tu nota:");
         double nota = lectorTeclado.nextDouble();
